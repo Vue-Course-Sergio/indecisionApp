113672e1f199c43efee6ea346fb84d1108c047b8
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-20 16:19:26 -0700

    split logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <h1>Mi primera app</h1>
   <hr />
-  <MyCounter :value="5" text="Hola mundo" />
-  <MyCounterScript :value="5" />
+  <MyCounter :value="1" text="Hola mundo" />
+  <MyCounterScript :value="2" />
+  <MyCounterScript2 :value="3" />
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
 import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
 </script>
