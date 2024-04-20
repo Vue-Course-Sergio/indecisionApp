3b52d60959a506ef8fe0fb70b64f3cb41f1823ec
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-20 15:42:08 -0700

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
-  <section>
-    <h3>Counter: {{ counter }}</h3>
-    <h3>Square: {{ squareCounter }}</h3>
-
-    <div>
-      <button @click="counter++">+1</button>
-      <button @click="counter--">-1</button>
-    </div>
-  </section>
+  <h1>Mi primera app</h1>
+  <hr />
+  <MyCounter />
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
-
-const counter = ref(2);
-
-const squareCounter = computed(() => counter.value * counter.value);
+import MyCounter from './components/MyCounter.vue';
 </script>
