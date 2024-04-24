4f8c51bb9bdc042c9763e3731e676734439741c0
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-24 10:48:16 -0700

    testing sum.ts

--- tests/helpers/sum.test.ts
+++ tests/helpers/sum.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum';
+import { sum, addArray } from '../../src/helpers/sum';
 
 test('adds 1 + 2 to equal 3', () => {
   const a = 1;
@@ -8,4 +8,18 @@ test('adds 1 + 2 to equal 3', () => {
   const result = sum(a, b);
 
   expect(result).toBe(a + b);
+});
+
+test('should return 0 if array is empty', () => {
+  const result = addArray([]);
+  expect(result).toBe(0);
+});
+
+test('should return proper value of addArray function', () => {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+
+  const result = addArray([a, b, c]);
+  expect(result).toBe(a + b + c);
 });
