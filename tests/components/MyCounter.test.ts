dfe771025e6a06657e37aff56129856e575ddfb9
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-24 20:52:40 -0700

    test trigger events

--- tests/components/MyCounter.test.ts
+++ tests/components/MyCounter.test.ts
@@ -26,4 +26,41 @@ describe('<MyCounter/>', () => {
     expect(counterLabel.text()).toContain(`Counter: ${value}`);
     expect(squareLabel.text()).toContain(`Square: ${value * value}`);
   });
+
+  test('increments the counter value when +1 buttin is clicked', async () => {
+    const value = 5;
+
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: value,
+      },
+    });
+
+    const btnIncrements = wrapper.find('button');
+    await btnIncrements.trigger('click');
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3');
+
+    expect(counterLabel.text()).toContain(`Counter: ${value + 1}`);
+    expect(squareLabel.text()).toContain(`Square: ${(value + 1) * (value + 1)}`);
+  });
+
+  test('decrements the counter value when -1 button is clicked twice', async () => {
+    const value = 5;
+
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: value,
+      },
+    });
+
+    const [_, btnDecrements] = wrapper.findAll('button');
+    await btnDecrements.trigger('click');
+    await btnDecrements.trigger('click');
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3');
+
+    expect(counterLabel.text()).toContain(`Counter: ${value - 2}`);
+    expect(squareLabel.text()).toContain(`Square: ${(value - 2) * (value - 2)}`);
+  });
 });
